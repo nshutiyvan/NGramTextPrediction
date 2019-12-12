68dc5689fd8f81b2c2e4a0122d9bf1912532b67e
Nshuti Yvan Christian <nshutiyvan@gmail.com>
2019-12-12 14:47:31 +0100

    Init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace NGramTextPredition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.numDepth = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numDepth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(237, 37);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 78);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(339, 335);
+            this.txtInput.TabIndex = 1;
+            // 
+            // numDepth
+            // 
+            this.numDepth.Location = new System.Drawing.Point(31, 40);
+            this.numDepth.Name = "numDepth";
+            this.numDepth.Size = new System.Drawing.Size(120, 20);
+            this.numDepth.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numDepth);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numDepth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.NumericUpDown numDepth;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NGramTextPredition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("");
+            System.Diagnostics.Debug.Write("Print Integral value:");
+            System.Diagnostics.Debug.Write(removeDirty(txtInput.Text));
+        }
+        private string removeDirty(string toCleanArray)
+        {
+            string result = "";
+            var charArray = toCleanArray.ToCharArray();
+            var charsToRemove = new string[] { "(", ",", ")" };
+            foreach (char c in charArray)
+            {
+                if (Char.IsLetter(c))
+                {
+                    result += Char.ToLower(c);
+                }
+                result += " ";
+
+            }
+            return result;
+
+
+        }
+    }
+}
